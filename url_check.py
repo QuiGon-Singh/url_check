e0f65d6929b09d90bbcc81ed4c6545e6b473c26a
QuiGon-Singh <gurps888@gmail.com>
2022-04-25 22:39:53 +0100

    Version 1

---
+++ url_check.py
@@ -0,0 +1,18 @@
+import sys
+
+from requests_html import HTMLSession
+
+
+def url_check(url, logger = None):
+
+    session = HTMLSession()
+
+    r = session.get(url)
+
+    if r.ok:
+        return(r)
+    else:
+        if not logger == None:
+            logger.critical(f'URL response code bad: {r.status_code}')
+        else:
+            sys.exit(f'URL response code bad: {r.status_code}')
